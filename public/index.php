a568b91cb9ae3a7e5c4a139d84d795a0cc1dc276
Arash Rahimi <rahimi18am@gmail.com>
2020-05-11 12:43:22 +0430

    this is last changes

--- public/index.php
+++ public/index.php
@@ -1,2 +1,33 @@
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
+
+$app = new \Slim\App();
+
+$countries = ['IRAN','USA','UK','INDIA'];
+
+
+$app->get('/',function ($request,$response,$args){
+    return $response->write('welcome to slim crash course');
+});
+
+$app->get('/countries',function ($request,$response,$args) use ($countries) {
+   return $response->withJson($countries);
+});
+
+function startWith($countries,$substr) {
+    $len = strlen($substr);
+    $filterdCountries = [];
+    foreach ($countries as $country) {
+        if ( substr(    strtolower($country),0,$len) ==    strtolower($substr) ) {
+            array_push($filterdCountries,$country);
+        }
+    }
+    return $filterdCountries;
+}
+
+$app->get('/countries/search',function ($request,$response,$args) use($countries) {
+   $term = $request->getQueryParams()['term'];
+   return $response->withJson(startWith($countries,$term));
+});
+
+$app->run();
